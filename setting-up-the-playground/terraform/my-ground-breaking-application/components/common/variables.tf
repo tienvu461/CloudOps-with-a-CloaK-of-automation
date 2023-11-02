606977ffc62c1e04b07eebdc1c0f85f387d3001c
tienvu461 <tienvu461@gmail.com>
2023-11-02 22:43:27 +0700

    add: basic setup

---
+++ setting-up-the-playground/terraform/my-ground-breaking-application/components/common/variables.tf
@@ -0,0 +1,6 @@
+variable "project_name" { default = "" }
+variable "env" { default = "" }
+variable "env_long" { default = "" }
+variable "aws_region" { default = "" }
+variable "aws_profile" { default = "" }
+variable "backend_bucket" { default = "" }
